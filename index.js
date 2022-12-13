9c970ee7e088dc6bc43247e9a8e8c2e9a62a4dc2
FrancoNDrago <108413642+FrancoNDrago@users.noreply.github.com>
2022-12-12 22:44:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class ProductManager {
-    static id = 0;
+    static id = 1;
     static products;
 
     constructor(title, description, price, thumbnail, code, stock) {
-      ProductManager.id +=1;
+      ProductManager.id +=0;
       this.products = [];
       this.title=title;
       this.description=description;
@@ -12,28 +12,69 @@ class ProductManager {
       this.code=code;
       this.stock=stock;
     }
+
     addProduct(product) {
-      this.product.push({title: this.title,description: this.description,price: this.price,
-      thumbnail: this.thumbnai,code: this.code,stock: this.stock});  
-      // if () {
-  
-      // }
+      product = { ...product, id: ProductManager.id };
+      let assignedCode = this.products.find(
+        (product) => product.code === this.code
+      );
+      if (assignedCode) {
+        return console.log(
+        "El codigo del producto", 
+        product.title, 
+        "tiene que ser distinto al de los demas productos"
+        );
+      } else {
+        this.products.push(product);
+        ProductManager.id++;
+      }
     }
+
     getProducts() {
       return this.products;
     }
-    getProductById(index) {
-      console.log("Id del producto:", find(index, 1).products);
-      // retornar el producto que cuente con este id
-      // pista: Utilizar find()
+
+    getProductById(id) {
+      let searchID = id;
+      let myProduct = null;
+      this.products.forEach((product) => {
+        if (product.id === searchID) {
+          myProduct = product;
+        }
+      });
+      if (myProduct === null) {
+        return console.log("Este id", searchID, "no fue encontrado");
+      } else {
+        return console.log("Este id", searchID, "pertenece al producto ", myProduct.title);
+      }
     }
   }
 
-  const product1 = new ProductManager("Camiseta","Camiseta de la selección",19.999,"url","ARG10",10);
-  product1.addProduct()
+const productList = new ProductManager();
 
-  const product2 = new ProductManager("Short","Short de la selección",8.999,"url","ARG11",10);
-  product2.addProduct()
 
-  const product3 = new ProductManager("Botines","Botines futbol 11",21.999,"url","ADI10",4);
-  product3.addProduct()
+const product1 = new ProductManager("Camiseta","Camiseta de la selección",19.999,"https://assets.adidas.com/images/w_600,f_auto,q_auto/d88ae2138faf49be8f74aeca012c62eb_9366/Camiseta_Titular_Argentina_22_Messi_Blanco_HL8425_01_laydown.jpg",
+"ARG10",10);
+
+const product2 = new ProductManager("Short","Short de la selección",8.999,"https://sporting.vtexassets.com/arquivos/ids/560737/6HK8071-000-1.jpg?v=637931679922730000",
+"ARG10",8);
+
+const product3 = new ProductManager("Botines","Botines futbol 11",21.999,"https://pbs.twimg.com/media/Fgv6gXUXkAE6Kg1?format=jpg&name=900x900",
+"ARG10",4);
+
+
+productList.addProduct(product1);
+productList.addProduct(product2);
+productList.addProduct(product3);
+
+
+console.log("Cargando productos... ");
+productList.addProduct(product1);
+productList.addProduct(product2);
+productList.addProduct(product3);
+
+console.log("Lista de productos: ");
+console.log(productList.getProducts());
+
+console.log("Producto con ID = 2");
+console.log(productList.getProductById(2));
