0b481de348e2a347a1a6154bbddac1cdf30fd3d4
FrancoNDrago <108413642+FrancoNDrago@users.noreply.github.com>
2022-12-19 19:26:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,37 @@ class ProductManager {
   constructor(title, description, price, thumbnail, code, stock) {
     ProductManager.id +=0;
     this.products = [];
-    this.title=title;
-    this.description=description;
-    this.price=price;
-    this.thumbnail=thumbnail;
-    this.code=code;
-    this.stock=stock;
+    //this.product = product;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
   }
 
   addProduct(product) {
-    product === { ...product, id: ProductManager.id };
+    product = { ...product, id: ProductManager.id };
     let assignedCode = this.products.find(
       (product) => {
+        return product.title === this.title;
+        return product.description === this.description;
+        return product.price === this.price;
+        return product.thumbnail === this.thumbnail;
         return product.code === this.code;
+        return product.stock === this.stock;
       }
     );
     if (assignedCode) {
       return console.log(
-       "El codigo del producto", 
-        product.title, 
-       "tiene que ser distinto al de los demas productos"
+       `El codigo del producto, 
+        ${product.code} 
+       tiene que ser distinto al de los demas productos`
       );
-    } else {
-      this.products.push(product);
+    } else product.id
+      this.getProducts().length + 1;this.products.push(product);
       ProductManager.id++;
-    }
+    
   }
 
   getProducts() {
@@ -54,14 +60,32 @@ class ProductManager {
 const productList = new ProductManager();
 
 
-const product1 = new ProductManager("Camiseta","Camiseta de la selección",19.999,"https://assets.adidas.com/images/w_600,f_auto,q_auto/d88ae2138faf49be8f74aeca012c62eb_9366/Camiseta_Titular_Argentina_22_Messi_Blanco_HL8425_01_laydown.jpg",
-"ARG10",10);
+const product1 = {
+  title: "Camiseta",
+  description: "Camiseta de la selección",
+  price: 19.999,
+  thumbnail: "https://assets.adidas.com/images/w_600,f_auto,q_auto/d88ae2138faf49be8f74aeca012c62eb_9366/Camiseta_Titular_Argentina_22_Messi_Blanco_HL8425_01_laydown.jpg",
+  code: "ARG10",
+  stock: 10,
+};
 
-const product2 = new ProductManager("Short","Short de la selección",8.999,"https://sporting.vtexassets.com/arquivos/ids/560737/6HK8071-000-1.jpg?v=637931679922730000",
-"ARG10",8);
+const product2 = {
+  title: "Short",
+  description: "Short de la seleccion",
+  price: 8.999,
+  thumbnail: "https://sporting.vtexassets.com/arquivos/ids/560737/6HK8071-000-1.jpg?v=637931679922730000",
+  code: "ARG10",
+  stock: 8,
+};
 
-const product3 = new ProductManager("Botines","Botines futbol 11",21.999,"https://pbs.twimg.com/media/Fgv6gXUXkAE6Kg1?format=jpg&name=900x900",
-"ARG10",4);
+const product3 = {
+  title: "Botines",
+  description: "Botines futbol 11",
+  price: 21.999,
+  thumbnail: "https://pbs.twimg.com/media/Fgv6gXUXkAE6Kg1?format=jpg&name=900x900",
+  code: "ARG10",
+  stock: 4,
+};
 
 
 productList.addProduct(product1);
